bcea000b64aeba2799fd7783a8728395bb8f58ee
Vicky Kumar <imvickykumar999@gmail.com>
2022-12-15 14:07:59 +0000

    self call

--- JSON Visualizer/myPackage/CallPretty.py
+++ JSON Visualizer/myPackage/CallPretty.py
@@ -4,15 +4,15 @@ def JsonPrint(response, response_Keys):
     print(response_Keys)
     for i in range(len(response_Keys)):
 
-        if type(response[response_Keys[i]]) == dict:
-            JsonPrint(response[response_Keys[i]], list(response[response_Keys[i]].keys()))
-
-        elif type(response[response_Keys[i]]) == list:
+        if type(response[response_Keys[i]]) == list:
             print()
             
             for j in range(len(response[response_Keys[i]])):
                 if type(response[response_Keys[i]][j]) == dict:
                     JsonPrint(response[response_Keys[i]][j], list(response[response_Keys[i]][j].keys()))
+
+        elif type(response[response_Keys[i]]) == dict:
+            JsonPrint(response[response_Keys[i]], list(response[response_Keys[i]].keys()))
 
         else:
             print(response_Keys[i], ':')
