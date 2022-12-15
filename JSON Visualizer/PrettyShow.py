c5425b2f7255f4b359383e3bdf6c62914cc2b8ab
Vicky Kumar <imvickykumar999@gmail.com>
2022-12-15 11:18:17 +0000

    specified code for given json

--- JSON Visualizer/PrettyShow.py
+++ JSON Visualizer/PrettyShow.py
@@ -2,10 +2,10 @@
 import requests, json
 
 BaseURL = "https://www.googleapis.com/youtube/v3/search?"
-part = "part=snippet"
-q = "&q=imvickykumar999"
-key = "&key=AIzaSyCcJX4qdbo9caqxZSKDmuBjNVWfvq8_Wcs"
-maxResults = "&maxResults=20"
+part = "part=" + "snippet"
+q = "&q=" + "imvickykumar999"
+key = "&key=" + "AIzaSyCcJX4qdbo9caqxZSKDmuBjNVWfvq8_Wcs"
+maxResults = "&maxResults=" + "20"
 
 payload={}
 headers = {}
@@ -13,6 +13,24 @@ url = BaseURL + part + q + key + maxResults
 response = requests.request("GET", url, headers=headers, data=payload)
 
 response = response.text
-response = json.dumps(response)
 response = json.loads(response)
-print(response)
+response = dict(response)
+response_Keys = list(response.keys())
+
+def JsonPrint(response_Keys):
+    for i in response_Keys:
+
+        if type(response[i]) == dict:
+            # JsonPrint(response_Keys)
+            print(i, '->', response[i])
+            print()
+
+        elif type(response[i]) == list:
+            for j in range(len(response[i])):
+                print(f'{i[:-1]} {j+1} ->', response[i][j])
+                print()
+
+        else:
+            print(i, '->', response[i])
+
+JsonPrint(response_Keys)
