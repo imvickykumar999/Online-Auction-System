caecf1ce8fd2be3db8d1b3677be4fe3019114b34
Vicky Kumar <imvickykumar999@gmail.com>
2022-12-20 14:44:25 +0530

    formatted

--- JSON Visualizer/myPackage/CallPretty.py
+++ JSON Visualizer/myPackage/CallPretty.py
@@ -2,12 +2,12 @@
 # Answered Stackoverflow Question : https://stackoverflow.com/a/74836817/11493297
 
 def JsonPrint(response, response_Keys):
-    print()
-    print(response_Keys)
+    print('\n', response_Keys)
+    print('---------------------------------\n')
     for i in range(len(response_Keys)):
 
         if type(response[response_Keys[i]]) == list:
-            print()
+            print('\n===========================', response_Keys[i])
             
             for j in range(len(response[response_Keys[i]])):
                 if type(response[response_Keys[i]][j]) == dict:
@@ -19,4 +19,4 @@ def JsonPrint(response, response_Keys):
         else:
             print(response_Keys[i], ':')
             for j in range(len(response[response_Keys[i]].split('\n'))):
-                print('\t', response[response_Keys[i]].split('\n')[j])
+                print('\t', response[response_Keys[i]].split('\n')[j], '\n')
--- JSON Visualizer/myPackage/test.py
+++ JSON Visualizer/myPackage/test.py
@@ -2,7 +2,16 @@
 import json
 import CallPretty as vix
 
-response = open('JSON Visualizer/sample.json')
+response = open('sample.json')
 response = json.load(response)
+
+# import requests
+# url = "https://arduino.esp8266.com/stable/package_esp8266com_index.json"
+# payload={}
+# headers = {}
+# response = requests.request("GET", url, headers=headers, data=payload)
+# response = response.text
+# response = json.loads(response)
+# response = dict(response)
 
 vix.JsonPrint(response, list(response.keys()))
